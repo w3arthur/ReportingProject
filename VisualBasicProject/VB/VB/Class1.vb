844e204ed60b9f7b457d9bdadf272181149a06ce
Arthur Zarankin <78190695+w3arthur@users.noreply.github.com>
2022-12-18 02:56:16 +0200

    Visual Basic Code

---
+++ VisualBasicProject/VB/VB/Class1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Class1
+    Function LongDate(AnyDate As Date) As String
+        Dim Suffix As String
+        Select Case Day(AnyDate)
+            Case 1, 21, 31
+                Suffix = "st"
+            Case 2, 22
+                Suffix = "nd"
+            Case 3, 23
+                Suffix = "rd"
+            Case Else
+                Suffix = "th"
+        End Select
+
+        Return Format(AnyDate, "dddd d") & Suffix & Format(AnyDate, " MMMM yyyy")
+    End Function
+End Class
